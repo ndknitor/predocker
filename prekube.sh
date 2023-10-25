b0aea337fdbfc3256e4061794ff96e9e1e2940d1
KN <64144072+ndknitor@users.noreply.github.com>
2023-10-25 14:13:53 +0700

    Create prekube.sh

---
+++ prekube.sh
@@ -0,0 +1,26 @@
+sudo swapoff -a \
+    && sudo cp -f /etc/fstab /etc/fstab.bak \
+    && sudo sed -e '/swap/ s/^#*/#/' -i /etc/fstab
+    
+sudo apt-get update && sudo app -y upgrade
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker ${USER}
+
+echo "net.bridge.bridge-nf-call-ip6tables=1" | sudo tee -a /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
