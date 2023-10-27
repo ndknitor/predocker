cbca8fe4ef48a22e22385bec5fd1a91d0615a987
KN <64144072+ndknitor@users.noreply.github.com>
2023-10-27 11:22:55 +0700

    Update prekube.sh

--- prekube.sh
+++ prekube.sh
@@ -12,15 +12,6 @@ sudo apt-get install -y \
     lsb-release
 
 
-
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" |sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt update
-sudo apt install containerd
-
-
-
-
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
