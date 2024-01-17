8ead053a802529a125f6510b93aed1beb4a90c85
KN <64144072+ndknitor@users.noreply.github.com>
2024-01-17 10:14:53 +0700

    Create main-master.sh

---
+++ main-master.sh
@@ -0,0 +1,52 @@
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+swapoff -a \
+    &&  cp -f /etc/fstab /etc/fstab.bak \
+    &&  sed -e '/swap/ s/^#*/#/' -i /etc/fstab
+
+ apt-get update &&  app -y upgrade
+
+ apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+ apt-get update
+ apt-get install -y containerd.io
+
+cat <<EOF |  tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF |  tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sysctl --system
+
+mkdir -p /etc/containerd
+containerd config default |  tee /etc/containerd/config.toml
+sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://docs.projectcalico.org/v3.17/manifests/calico.yaml
+kubectl get nodes
