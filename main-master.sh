46446a6fbec79e6d21e0fcf5fb2f8f133a2626c3
KN <64144072+ndknitor@users.noreply.github.com>
2024-01-18 12:01:29 +0700

    Update main-master.sh

--- main-master.sh
+++ main-master.sh
@@ -6,16 +6,14 @@ fi
 apt-get update &&  apt -y upgrade
 apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
-swapoff -a \
-    &&  cp -f /etc/fstab /etc/fstab.bak \
-    &&  sed -e '/swap/ s/^#*/#/' -i /etc/fstab
+swapoff -a
+cp -f /etc/fstab /etc/fstab.bak
+sed -e '/swap/ s/^#*/#/' -i /etc/fstab
 
 curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
- apt-get update
- apt-get install -y containerd.io
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y containerd.io
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
