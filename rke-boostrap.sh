14cc83ef53bb1fab274df80a6c48a16cb4c551c2
KN <64144072+ndknitor@users.noreply.github.com>
2024-01-25 09:27:31 +0700

    Create rke-boostrap.sh

---
+++ rke-boostrap.sh
@@ -0,0 +1,20 @@
+#Disable swap
+sudo swapoff -a
+sudo cp -f /etc/fstab /etc/fstab.bak
+sudo sed -e '/swap/ s/^#*/#/' -i /etc/fstab
+
+#Install package
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+#Install Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker ${USER}
+
+#Install IP forwarding
+echo "net.bridge.bridge-nf-call-ip6tables=1" | sudo tee -a /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
