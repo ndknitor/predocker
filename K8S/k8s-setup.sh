49e02857039fc38c116b5010ce01b5d6ae07e334
KN <64144072+ndknitor@users.noreply.github.com>
2025-09-10 17:56:25 +0700

    Update k8s-setup.sh

--- K8S/k8s-setup.sh
+++ K8S/k8s-setup.sh
@@ -32,11 +32,11 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 apt-get update
 apt-get install -y containerd.io
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 if [ "$is_master_node" = true ]; then
-    apt-get install -y kubeadm
+    apt-get install -y kubelet
     apt-mark hold kubeadm kubelet
 else
     apt-get install -y kubeadm kubelet
